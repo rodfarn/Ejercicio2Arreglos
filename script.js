916f3f7955ed146ffdff72999b7bf2f4e4e10673
rodfarn <r.farfan120398@gmail.com>
2024-03-10 15:03:04 -0600

    Ejercicio 2: Arreglos

---
+++ script.js
@@ -0,0 +1,33 @@
+//Ejercicio 1
+
+/*
+let arreglo = []; // Creando el arreglo vacío
+let usuarioInput; // Creando la variable usuarioInput
+
+//Ciclo do-while
+do {
+    usuarioInput = prompt("Ingresa un caracter")
+    arreglo.push(usuarioInput)
+} while (arreglo.length < 10)
+
+console.log(arreglo);
+*/
+
+//Ejercicio 2
+/*
+let usuarioString = prompt("Ingresa un carácter");
+
+console.log(usuarioString.split(","));
+*/
+
+//Ejercicio 3
+/*
+let arrayDado = [10, 40, 30, 20, 15, 5];
+arrayDado.sort(function (a, b) {
+    return a - b;
+});
+
+console.log(arrayDado);
+console.log(`El número más pequeño es ${Math.min(...arrayDado)}`)
+console.log(`El número más grande es ${Math.max(...arrayDado)}`)
+*/
